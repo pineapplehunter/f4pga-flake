f72669150cee7ef30578945f2b55224ddb1a6870
pi-coding-agent <peshogo+agent@gmail.com>
2026-09-24 11:14:36 +0900

    flake: migrate to flake-parts
    Use the perSystem structure from the nixos-config templates and replace treefmt-nix with nixfmt-tree.

--- flake.nix
+++ flake.nix
@@ -1,39 +1,20 @@
 {
-  description = "A very basic flake";
+  description = "A reproducible F4PGA environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-f4pga.url = "github:nixos/nixpkgs/5e4fbfb6b3de1aa2872b76d49fafc942626e2add";
     systems.url = "github:nix-systems/default-linux";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-f4pga,
-      systems,
-      treefmt-nix,
-    }:
+    { flake-parts, ... }@inputs:
     let
-      lib = nixpkgs.lib;
-      eachSystem = lib.genAttrs (import systems);
-      pkgsFor =
-        system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-    in
-    {
-      overlays.default =
+      overlay =
         final: prev:
         let
-          f4pgaToolchain = import nixpkgs-f4pga {
+          f4pgaToolchain = import inputs.nixpkgs-f4pga {
             system = final.stdenv.hostPlatform.system;
           };
         in
@@ -80,94 +61,89 @@
           f4pga-arch-defs = final.callPackages ./packages/f4pga-arch-defs.nix { };
           f4pga = final.python3Packages.toPythonApplication final.python3Packages.f4pga;
         };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-      packages = eachSystem (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
+      flake.overlays.default = overlay;
+
+      perSystem =
         {
-          inherit (pkgs)
-            f4pga
-            prjxray-config
-            prjxray-tools
-            vtr
-            ;
-          inherit (pkgs.python3.pkgs)
-            fasm
-            prjxray
-            ql-fasm
-            ql-fasm-utils
-            qlf-fasm
-            quicklogic-timings-importer
-            tinyfpgab
-            xc-fasm
-            ;
-        }
-      );
-
-      checks = eachSystem (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
+          config,
+          pkgs,
+          system,
+          ...
+        }:
         {
-          vtr-no-gui = self.packages.${system}.vtr.override { enableX11 = false; };
-          xc7-bitstream = pkgs.callPackage ./tests/xc7-bitstream.nix { };
-          xc7-bitstream-arty-a7-100t = pkgs.callPackage ./tests/xc7-bitstream.nix {
-            archDef = pkgs.f4pga-arch-defs.xc7a100t_test;
-            board = "arty-a7-100t";
-            source = ./examples/arty-a7-100t;
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
           };
-        }
-        // self.packages.${system}
-      );
 
-      devShells = eachSystem (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
-        rec {
-          xc7 = pkgs.callPackage ./env/common.nix { family = "xc7"; };
-          xc7a50t = pkgs.callPackage ./env/common.nix {
-            family = "xc7";
-            allDevices = false;
-            enableXc7a50t = true;
+          packages = {
+            inherit (pkgs)
+              f4pga
+              prjxray-config
+              prjxray-tools
+              vtr
+              ;
+            inherit (pkgs.python3.pkgs)
+              fasm
+              prjxray
+              ql-fasm
+              ql-fasm-utils
+              qlf-fasm
+              quicklogic-timings-importer
+              tinyfpgab
+              xc-fasm
+              ;
           };
-          xc7a100t = pkgs.callPackage ./env/common.nix {
-            family = "xc7";
-            allDevices = false;
-            enableXc7a100t = true;
-          };
-          xc7a200t = pkgs.callPackage ./env/common.nix {
-            family = "xc7";
-            allDevices = false;
-            enableXc7a200t = true;
-          };
-          xc7a010t = pkgs.callPackage ./env/common.nix {
-            family = "xc7";
-            allDevices = false;
-            enableXc7a010t = true;
-          };
-          eos-s3 = pkgs.callPackage ./env/common.nix { family = "eos-s3"; };
-          ql-eos-s3_wlcsp = pkgs.callPackage ./env/common.nix {
-            family = "eos-s3";
-            allDevices = false;
-            enableQlEosS3Wlcsp = true;
-          };
-          default = xc7;
-        }
-      );
 
-      formatter = eachSystem (
-        system:
-        (treefmt-nix.lib.evalModule (pkgsFor system) {
-          projectRootFile = "flake.nix";
-          programs.nixfmt.enable = true;
-        }).config.build.wrapper
-      );
+          checks = {
+            vtr-no-gui = pkgs.vtr.override { enableX11 = false; };
+            xc7-bitstream = pkgs.callPackage ./tests/xc7-bitstream.nix { };
+            xc7-bitstream-arty-a7-100t = pkgs.callPackage ./tests/xc7-bitstream.nix {
+              archDef = pkgs.f4pga-arch-defs.xc7a100t_test;
+              board = "arty-a7-100t";
+              source = ./examples/arty-a7-100t;
+            };
+          }
+          // config.packages;
 
-      legacyPackages = eachSystem pkgsFor;
+          devShells = rec {
+            xc7 = pkgs.callPackage ./env/common.nix { family = "xc7"; };
+            xc7a50t = pkgs.callPackage ./env/common.nix {
+              family = "xc7";
+              allDevices = false;
+              enableXc7a50t = true;
+            };
+            xc7a100t = pkgs.callPackage ./env/common.nix {
+              family = "xc7";
+              allDevices = false;
+              enableXc7a100t = true;
+            };
+            xc7a200t = pkgs.callPackage ./env/common.nix {
+              family = "xc7";
+              allDevices = false;
+              enableXc7a200t = true;
+            };
+            xc7a010t = pkgs.callPackage ./env/common.nix {
+              family = "xc7";
+              allDevices = false;
+              enableXc7a010t = true;
+            };
+            eos-s3 = pkgs.callPackage ./env/common.nix { family = "eos-s3"; };
+            ql-eos-s3_wlcsp = pkgs.callPackage ./env/common.nix {
+              family = "eos-s3";
+              allDevices = false;
+              enableQlEosS3Wlcsp = true;
+            };
+            default = xc7;
+          };
+
+          formatter = pkgs.nixfmt-tree;
+
+          legacyPackages = pkgs;
+        };
     };
 }
